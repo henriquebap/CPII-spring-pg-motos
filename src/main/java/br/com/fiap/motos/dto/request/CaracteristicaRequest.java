8ff557c560cdcf219d625fcf7cbff56f3cab3a3e
Henriquebap <henriquebaptista2003@gmail.com>
2024-04-24 22:36:39 -0300

    Caracteristica Request BUG

--- src/main/java/br/com/fiap/motos/dto/request/CaracteristicaRequest.java
+++ src/main/java/br/com/fiap/motos/dto/request/CaracteristicaRequest.java
@@ -4,9 +4,15 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CaracteristicaRequest(
-        @Size(min = 5, max = 255, message = "Nome deve ter entre 5 e 255 caracteres!") @NotNull(message = "Nome é obrigatório!") String nome,
+                @Size(min = 5, max = 255, message = "Nome deve ter entre 5 e 255 caracteres!") @NotNull(message = "Nome é obrigatório!") String nome,
 
-        @Size(min = 5, max = 255, message = "Descrição deve ter entre 5 e 255 caracteres!") @NotNull(message = "Descrição é obrigatória!") String descricao,
+                @Size(min = 5, max = 255, message = "Descrição deve ter entre 5 e 255 caracteres!") @NotNull(message = "Descrição é obrigatória!") String descricao,
 
-        @NotNull(message = "ID do veículo é obrigatório!") AbstractRequest veiculoId) {
+                @NotNull(message = "ID do veículo é obrigatório!") AbstractRequest veiculoId) {
+
+        public CaracteristicaRequest {
+                if (veiculoId == null) {
+                        throw new IllegalArgumentException("ID do veículo é obrigatório!");
+                }
+        }
 }
